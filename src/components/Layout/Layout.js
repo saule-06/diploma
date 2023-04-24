c3c735609bd7c13362f26a54a8cbf964e865446b
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-04-24 13:41:05 +0600

    Added a new department for ordering

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import "./Layout.css";
 import Logo from "../Logo/Logo";
 import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
+import Auth from "../Auth/Auth";
 
 export default function Layout(props) {
   return (
@@ -10,6 +11,7 @@ export default function Layout(props) {
       <header>
         <Nav />
         <CartLink />
+        <Auth />
       </header>
       <aside>
         <Logo />
