b78352ad091eeab11ecaac8bb887dc3c83647cc9
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-01 15:34:29 +0600

    Add Auth component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,25 @@
+import "./Auth.css";
 import { useContext } from "react";
 import { logIn, logOut } from "../../firebase";
 import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
 
   return (
     <div className="Auth">
-      {!user ? <button onClick={logIn}>Sign in</button> : null}
-      {user ? <span>{user.displayName}</span> : null}
-      {user ? <button onClick={logOut}>Sign out</button> : null}
+      {user ? (
+        <span>
+          Hello <Link to="/orders">{user.displayName}</Link>!
+          <button className="Test" onClick={logOut}>Sign out</button>
+        </span>
+      ) : (
+        <span>
+          Hello guest!
+          <button className="Test" onClick={logIn}>Sign in</button>
+        </span>
+      )}
     </div>
   );
-} 
+}
