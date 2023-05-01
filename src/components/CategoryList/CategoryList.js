471571e14efecb7c05716134e3f856c2a4f5a136
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-01 15:31:20 +0600

    Add category component

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,9 +1,10 @@
 
 import { NavLink } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AppContext } from "../../App";
-import Product from "../../pages/Product";
 import "./CategoryList.css";
+import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
@@ -14,12 +15,14 @@ export default function CategoryList() {
       <NavLink to={`/categories/${category.slug}`}>
         {category.name}
       </NavLink>
+      <DeleteCategory />
     </li>
   ));
   return (
     <div className="CategoryList">
       <h3>Categories</h3>
       <ul>{output}</ul>
+      <AddCategory />
     </div>
   );
 }
