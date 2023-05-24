0a1cbc68a0ab9e42301d75806220deefe4ef6a8e
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-24 10:42:42 +0600

    Add a new component burger

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,20 @@
+
+import Nav from "../../Nav/Nav";
+import Logo from "../Logo/Logo";
+import "./Drawer.css"
+
+function Drawer({ open, toggle }) {
+  const drawerClassNames = `Drawer ${open ? "open" : ""}`;
+
+  return (
+    <div className={drawerClassNames}>
+      <div onClick={toggle} className="backdrop"></div>
+      <div className="content">
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
+} 
+
+export default Drawer; 
---
+++ src/components/NavToggle/NavToggle.js
@@ -0,0 +1,14 @@
+import "./NavToggle.css";
+
+function NavToggle({ callback }) {
+  return (
+    <button onClick={callback} className="NavToggle">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div> 
+      <div className="line"></div>                
+    </button>
+  );
+}
+
+export default NavToggle;
