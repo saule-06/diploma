3beafeafd654abd9546af13aebf81cf4e957c202
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 00:38:43 +0600

    set styles to main in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,24 +1,37 @@
-import Nav from "../../Nav/Nav";
-import "./Layout.css";
-import Logo from "../Logo/Logo";
-import CategoryList from "../CategoryList/CategoryList";
-import CartLink from "../CartLink/CartLink";
+import { useState } from "react";
 import Auth from "../Auth/Auth";
+import CartLink from "../CartLink/CartLink";
+import CategoryList from "../CategoryList/CategoryList";
+import Drawer from "../Drawer/Drawer";
+import Footer from "../Footer/Footer";
+import Logo from "../Logo/Logo";
+import NavToggle from "../NavToggle/NavToggle";
+import "./Layout.css";
+import Nav from "../../Nav/Nav";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
-        <CartLink />
-        <Auth />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
+        <div className="user">
+          <CartLink />
+          <Auth />
+        </div>
       </header>
       <aside>
         <CategoryList />
       </aside>
       <main>{props.children}</main>
-      <footer>FOOTER</footer>
+      <Footer />
     </div>
   );
 }
