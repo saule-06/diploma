4f1d824efabb63cfde3fd9c638827bf6267329f2
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 13:26:31 +0600

    added a new element for the home category

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
+
+
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
-      <p>This is my home page</p>
+      <h1>Sweet Thomas</h1>
+      <img src={"https://conterfrag.ru/wp-content/uploads/2025/08/doughnuts-1868573_640.jpg"} alt="image" />
+      <div className="catochka">
+        <img className="sweet" src="https://i.pinimg.com/originals/86/f7/9c/86f79cbeb584b68afe4c70cf24b4d252.jpg" alt="sweet" />
+        <img className="sweet1" src="https://gagaru.club/uploads/posts/2023-02/1675916214_gagaru-club-p-pechene-raznotsvetnoe-makarons-instagram-2.jpg" alt="sweet1" /> 
+      </div>
     </div>
   );
 }
