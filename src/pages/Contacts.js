27338f10668afe03e7e121390735f6dcefaf3a29
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 13:58:08 +0600

    in the contact category added a delivery address

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,8 +1,21 @@
 export default function Contacts() {
   return (
-    <div className="Contacts">
-      <h1>Contacts</h1>
-      <p>This is my home page</p>
+    <div className="contacts-container container">
+      <h2 className="contacts-heading">Contact us</h2>
+      <div className="contact-details">
+        <p>
+          <strong className="contact-label">Address:</strong> Kyrgyzstan, Karakol, Esenina 1 
+        </p>
+        <p>
+          <strong className="contact-label">Phone:</strong>+996 707 518571
+        </p>
+        <p>
+          <strong className="contact-label">Email:</strong> tojgonbaevasaule@gmail.com
+        </p>
+        <p>
+          <strong className="contact-label">Working Hours:</strong> Monday to Friday, 9:00 AM - 8:00 PM
+        </p>
+      </div>
     </div>
   );
 }
