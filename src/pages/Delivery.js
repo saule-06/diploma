452734d58d515f1186489724a8fd3f130ea3ff68
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 15:25:29 +0600

    added new properties in the delivery category

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,8 +1,48 @@
-export default function Delivery() {
+
+const Delivery = () => {
   return (
-    <div className="Delivery">
-      <h1>Delivery</h1>
-      <p>This is my home page</p>
+    <div className="delivery-container">
+      <h1 className="delivery-heading">Delivery</h1>
+      <p className="delivery-text">
+      In this article, I provide instructions for cafes and restaurants on how to promote the food delivery service on the Internet. How to get orders, what to look for, where there are risks of losing the budget and how to avoid if.
+      </p>
+      <p className="delivery-text">
+        
+      </p>
+      <p className="delivery-text">
+      Why is the instruction secret? Because this is the internal regulation of our company. We simplified it somewhere, supplemented it somewhere for blog readers. In addition, there has never been such a plan to promote food delivery on the Internet.
+      </p>
+      <p className="delivery-text">
+      As the entire world went into self-isolation, the need for food and groceries delivery increased dramatically. 
+      </p>
+      <div className="delivery-info">
+        <div className="delivery-option">
+          <i className="fas fa-shipping-fast"></i>
+          <h3 className="delivery-option-heading">Fast Delivery</h3>
+          <p className="delivery-option-text">
+            Get your order delivered quickly and efficiently with our expedited
+            shipping options.
+          </p>
+        </div>
+        <div className="delivery-option">
+          <i className="fas fa-shield-alt"></i>
+          <h3 className="delivery-option-heading">Secure Delivery</h3>
+          <p className="delivery-option-text">
+            Rest assured that your package will be delivered securely and
+            protected process.
+          </p>
+        </div>
+        <div className="delivery-option">
+          <i className="fas fa-box"></i>
+          <h3 className="delivery-option-heading">Track Your Order</h3>
+          <p className="delivery-option-text">
+            Easily track your order in real-time and stay updated on its
+            delivery status.
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Delivery;
