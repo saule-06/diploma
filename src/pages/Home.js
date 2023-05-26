cb937e01ee3cebb8d682a86ff7d7d2308ab80657
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 15:35:50 +0600

    fixed netlify problem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Sweet Thomas</h1>
-      <img src={"https://conterfrag.ru/wp-content/uploads/2025/08/doughnuts-1868573_640.jpg"} alt="image" />
+      <img src={"https://conterfrag.ru/wp-content/uploads/2025/08/doughnuts-1868573_640.jpg"} alt="cake" />
       <div className="catochka">
         <img className="sweet" src="https://i.pinimg.com/originals/86/f7/9c/86f79cbeb584b68afe4c70cf24b4d252.jpg" alt="sweet" />
         <img className="sweet1" src="https://gagaru.club/uploads/posts/2023-02/1675916214_gagaru-club-p-pechene-raznotsvetnoe-makarons-instagram-2.jpg" alt="sweet1" /> 
