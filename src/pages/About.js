63051d44ebe63f73c473a9395898490ab0af2ea5
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-05-26 13:55:58 +0600

    added new information to the site about us

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,7 @@ export default function About() {
         <img src={"https://i.pinimg.com/564x/ed/0e/d6/ed0ed629bf0d433de304856754abe4fa.jpg"} alt="img" />
         <p>The English word "dessert" is derived from French desservir..Historians normally agree that dried fruits and honey were the first sweeteners after put in other dishes to make them sweet. Ancient civilizations in Mesopotamia and India prepared sweets as their gifts to the gods.
 Nonetheless, the discovery and spread of sugarcane marks the most important moment in the history of desserts. The Indians grew it 2,500 years ago, but it was only in 500 AD that sugar was crystalized and traded to China.</p>
+
       </div>
     </div>
   );
